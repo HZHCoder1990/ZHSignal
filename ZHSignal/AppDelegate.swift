66943b9780551e4000540697e6869bcb35fa60cb
HZHCoder1990 <2282802627@qq.com>
2024-07-08 20:56:41 +0800

    feat: 配置启动项

--- ZHSignal/AppDelegate.swift
+++ ZHSignal/AppDelegate.swift
@@ -10,8 +10,23 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    // MARK: - Constants
+    private enum Constants {
+        static let appLaunchesAttemptedKey = "AppLaunchesAttemptedKey"
+    }
+    
+    // MARK: - Lifecycle
+    
+    
+    
+    
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // 记录启动时间
+        let launchStartedAt = CACurrentMediaTime()
+        
+        
     
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = ViewController()
